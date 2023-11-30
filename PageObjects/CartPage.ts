1f12d3c3c76461b7d450a6b72c2a9447cdea6957
AlexandraLindau <alexandra.shokhan@gmail.com>
2023-11-30 19:52:47 +0100

    Replace object parameter with an interface

--- PageObjects/CartPage.ts
+++ PageObjects/CartPage.ts
@@ -2,11 +2,12 @@ import BasePage from './BasePage.js';
 import HeaderComponent from './Components/HeaderComponent.js';
 import { Page } from 'playwright-core';
 import CartItemComponent from './Components/CartItemComponent.js';
+import IProduct from '../models/IProduct.js';
 
 class CartPage extends BasePage {
     headerComponenet: HeaderComponent;
     cartItemComponent: CartItemComponent;
-    product: { title: string; quantity: string; description: string; price: string; button: string; };
+    product: IProduct;
     constructor(page: Page) {
         super(page);
         this.endpoint = 'cart.html';
--- models/IProduct.ts
+++ models/IProduct.ts
@@ -1,7 +1,9 @@
 interface IProduct {
     title: string,
     description: string,
-    price: string
+    price: string,
+    quantity?: string,
+    button?: string
 }
 
 export default IProduct;
