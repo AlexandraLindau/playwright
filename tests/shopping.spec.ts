7ccb547aeafae8e1a9ba2927c9a8017d6fc0600b
AlexandraLindau <alexandra.shokhan@gmail.com>
2023-12-05 19:55:56 +0100

    Add 'Add random product to cart'

--- tests/shopping.spec.ts
+++ tests/shopping.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from './fixture';
 import 'dotenv/config'
+import IProduct from '../models/IProduct';
 
 test.beforeEach(async ({ page, loginPage }) => {
   await page.goto('');
@@ -22,4 +23,29 @@ test('Add product to cart', async ({ productsPage, cartPage }) => {
   cartPage.cartItemComponent.removeProductByIndex(0);
   await expect(cartPage.headerComponenet.shoppingCartBadge).toBeHidden();
   await expect(cartPage.cartItemComponent.products).toBeHidden();
+});
+
+test('Add random products to cart', async ({ productsPage, cartPage }) => {
+  const maxNumber = (await productsPage.inventoryItemComponent.getProductsList()).length;
+  let numberOfProducts = Math.floor(Math.random() * maxNumber) + 1;
+  let expectedProducts: IProduct[] = [];
+  let actualProducts: IProduct[] = [];
+
+  while (numberOfProducts > 0) {
+    let itemIndex = Math.floor(Math.random() * 6);
+    if (await productsPage.inventoryItemComponent.getProductButton(itemIndex).innerText() === 'Add to cart') {
+      productsPage.inventoryItemComponent.addItemToCartByIndex(itemIndex);
+      expectedProducts.push(await productsPage.inventoryItemComponent.getProductByIndex(itemIndex));
+      numberOfProducts--;
+    }
+  }
+
+  await productsPage.headerComponent.openCart();
+  const numberOfProductsInCart = (await cartPage.cartItemComponent.getProductsList()).length;
+  for (let i = 0; i < numberOfProductsInCart; i++) {
+    actualProducts.push(await cartPage.cartItemComponent.getProductByIndex(i));
+  }
+
+  expect(actualProducts).toEqual(expectedProducts);
+
 })
